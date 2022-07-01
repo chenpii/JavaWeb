04809f20ff505bb0baefb90bd84c2e1f705da5d0
chenh <641760512@qq.com>
2022-07-01 17:46:12 +0800

    servlet 映射

---
+++ javaweb-02-servlet/servlet-01/src/main/java/com/kuang/servlet/ErrorServlet.java
@@ -0,0 +1,26 @@
+package com.kuang.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author chenpi
+ * @create 2022-07-01 16:40
+ */
+public class ErrorServlet extends HelloServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>404<h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
