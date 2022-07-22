51ac5dc4bd837d89f9d1cc9bfb48a434534d6603
chenh <641760512@qq.com>
2022-07-22 14:01:32 +0800

    jdbc 加载驱动-创建连接-执行sql语句获得结果集-遍历结果集-关闭资源

--- javaweb-jdbc/src/main/java/com/kuang/test/TestJdbc.java
+++ javaweb-jdbc/src/main/java/com/kuang/test/TestJdbc.java
@@ -12,7 +12,7 @@ public class TestJdbc {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         //配置信息
         //useUnicode=true&characterEncoding=utf-8 解决中文乱码
-        String url = "jdbc:mysql://150.158.162.234:3306/smbms?jdbc?useUnicode=true&characterEncoding=utf-8";
+        String url = "jdbc:mysql://150.158.162.234:3306/smbms?jdbc?useUnicode=true&characterEncoding=utf-8&useSSL=false";
         String username = "root";
         String password = "310012";
 
@@ -25,10 +25,23 @@ public class TestJdbc {
         Statement statement = connection.createStatement();
 
         //4.编写SQL
-        String sql = "select * from user";
+        String sql = "select * from users";
 
         //5.执行查询sql，返回一个结果集
         ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            System.out.println("id:" + resultSet.getObject("id"));
+            System.out.println("name:" + resultSet.getObject("name"));
+            System.out.println("password:" + resultSet.getObject("password"));
+            System.out.println("email:" + resultSet.getObject("email"));
+            System.out.println("birthday:" + resultSet.getObject("birthday"));
+        }
+
+        //6.关闭连接释放资源(一定要做）先开后关
+        resultSet.close();
+        statement.close();
+        connection.close();
 
 
     }
