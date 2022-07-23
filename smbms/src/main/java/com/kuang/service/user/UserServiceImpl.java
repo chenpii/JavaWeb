a4b9fa002d6af75be549cefe7e26612f469c4967
chenh <641760512@qq.com>
2022-07-23 23:29:32 +0800

    获取登录用户的业务层

--- smbms/src/main/java/com/kuang/service/user/UserServiceImpl.java
+++ smbms/src/main/java/com/kuang/service/user/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.kuang.service.user;
 
+import com.kuang.dao.BaseDao;
 import com.kuang.dao.user.UserDao;
 import com.kuang.dao.user.UserDaoImpl;
 import com.kuang.pojo.User;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * @author chenpi
@@ -20,6 +25,23 @@ public class UserServiceImpl implements UserService {
 
 
     public User login(String userCode, String password) {
-        return null;
+        Connection connection = null;
+        User user = null;
+        connection = BaseDao.getConnection();
+        try {
+            user = userDao.getLoginUser(connection, userCode);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            BaseDao.closeResource(connection, null, null);
+        }
+        return user;
+    }
+
+    @Test
+    public void test() {
+        UserServiceImpl userService = new UserServiceImpl();
+        User admin = userService.login("admin", "88888888");
+        System.out.println(admin.getUserPassword());
     }
 }
