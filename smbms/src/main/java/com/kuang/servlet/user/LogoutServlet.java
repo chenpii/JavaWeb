dccf717dee09cce9b6bfcad8cdd05f53c67078e9
chenh <641760512@qq.com>
2022-07-25 17:40:34 +0800

    注销登录

---
+++ smbms/src/main/java/com/kuang/servlet/user/LogoutServlet.java
@@ -0,0 +1,28 @@
+package com.kuang.servlet.user;
+
+import com.kuang.util.Constants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author chenpi
+ * @create 2022-07-25 16:57
+ */
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //移除用户的USER_SESSION
+        req.getSession().removeAttribute(Constants.USER_SESSION);
+        //返回登录界面
+        resp.sendRedirect(req.getContextPath() + "/login.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
