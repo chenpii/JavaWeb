a30061b8fe2553213bc1b49c17f6473b3a443ff9
chenp <641760512@qq.com>
2022-08-12 16:38:15 +0800

    文件上传

---
+++ 功能扩展/file/src/java/com/kuang/jmeter/RandomStr.java
@@ -0,0 +1,127 @@
+package com.kuang.jmeter;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class RandomStr {
+    public String getRandomStr(int leng) {
+        UUID uuid = UUID.randomUUID();//获取UID的值
+        System.out.println(uuid);
+        //去掉“-”
+        String uuidSub = uuid.toString().replaceAll("-", "").substring(0, leng);
+        return uuidSub;
+    }
+
+    public String getRandomIdCard() {
+
+        StringBuilder generater = new StringBuilder();
+        int sex = 0; // 1为男 0 为女
+        int age = 1979; //1979为大于18岁 2000小于18岁
+
+        //地区号
+        Map areaCode = new HashMap();
+        areaCode.put("浙江省", 330000);
+        areaCode.put("杭州市", 330100);
+        areaCode.put("市辖区", 330101);
+        areaCode.put("上城区", 330102);
+        areaCode.put("下城区", 330103);
+        areaCode.put("江干区", 330104);
+        areaCode.put("拱墅区", 330105);
+        areaCode.put("西湖区", 330106);
+        areaCode.put("滨江区", 330108);
+        areaCode.put("萧山区", 330109);
+        areaCode.put("余杭区", 330110);
+        areaCode.put("桐庐县", 330122);
+        areaCode.put("淳安县", 330127);
+        areaCode.put("建德市", 330182);
+        areaCode.put("富阳市", 330183);
+        areaCode.put("临安市", 330185);
+        String randomAreaCode = "";
+        int index = (int) (Math.random() * areaCode.size());
+        Collection values = areaCode.values();
+        Iterator it = values.iterator();
+        int i = 0;
+        int code = 0;
+        while (i < index && it.hasNext()) {
+            i++;
+            randomAreaCode = it.next().toString();
+        }
+        generater.append(randomAreaCode);
+
+        //生日
+        String randomBirthday = "";
+        Calendar birthday = Calendar.getInstance();
+        birthday.set(Calendar.YEAR, (int) (Math.random() * 20) + age);
+        birthday.set(Calendar.MONTH, (int) (Math.random() * 12));
+        birthday.set(Calendar.DATE, (int) (Math.random() * 31));
+        StringBuilder builder = new StringBuilder();
+        builder.append(birthday.get(Calendar.YEAR));
+        long month = birthday.get(Calendar.MONTH) + 1;
+        if (month < 10) {
+            builder.append("0");
+        }
+        builder.append(month);
+        long date = birthday.get(Calendar.DATE);
+        if (date < 10) {
+            builder.append("0");
+        }
+        builder.append(date);
+        randomBirthday = builder.toString();
+        generater.append(randomBirthday);
+
+        //随机码
+        String randomCode = "";
+        int code = (int) (Math.random() * 1000);
+        if (code < 10) {
+            //  randomCode= "00" + code;
+            if (code % 2 == sex) {
+                randomCode = "00" + code;
+            } else {
+                code = code + 1;
+                randomCode = "00" + code;
+            }
+        } else if (code < 100) {
+            // randomCode= "0" + code;
+            if (code % 2 == sex) {
+                randomCode = "0" + code;
+            } else {
+                code = code + 1;
+                randomCode = "0" + code;
+            }
+        } else {
+            // randomCode= "" + code;
+            if (code % 2 == sex) {
+                randomCode = "" + code;
+            } else {
+                code = code + 1;
+                randomCode = "" + code;
+            }
+        }
+        generater.append(randomCode);
+
+        //验证码
+        char[] chars = generater.toString().toCharArray();
+        int[] c = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+        char[] r = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+        int[] n = new int[17];
+        int result = 0;
+        for (int i = 0; i < n.length; i++) {
+            n[i] = Integer.parseInt(chars[i] + "");
+        }
+        for (int i = 0; i < n.length; i++) {
+            result += c[i] * n[i];
+        }
+        char validateCode = r[result % 11];
+        generater.append(validateCode);
+        vars.put("ldcard", generater.toString());
+        SampleResult.setResponseData(generater.toString());
+    }
+
+    @Test
+    public void test() {
+
+        System.out.println(getRandomStr(6));
+        System.out.println(getRandomStr(7));
+    }
+}
--- 功能扩展/file/src/java/com/kuang/servlet/FileServlet.java
+++ 功能扩展/file/src/java/com/kuang/servlet/FileServlet.java
@@ -1,13 +1,42 @@
 package com.kuang.servlet;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 public class FileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //判断上传的的文件是普通表单还是文件表单
+        //isMultipartContent判断请求消息中的内容是否是“multipart/form-data”类型，是则返回true，否则返回false
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            return;//说明是个普通表单，直接返回
+        }
+
+        //创建上传文件的保存路径，建议在WEB-INF路径下，安全，用户无法直接访问上传的文件；
+        String uploadPath = this.getServletContext().getRealPath("/WEB-INF/upload");
+        File uploadFile = new File(uploadPath);
+        if (!uploadFile.exists()) {
+            uploadFile.mkdir();//创建目录
+        }
+
+        //缓存，临时路径，加入文件超过了预期的大小，我们就把他放到一个临时文件中，过几天自动删除，或者提醒用户转存为永久
+        String tmpPath = this.getServletContext().getRealPath("/WEB-INF/tmp");
+        File tmpPFile = new File(tmpPath);
+        if (!tmpPFile.exists()) {
+            tmpPFile.mkdir();//创建目录
+        }
+        //处理上传的文件，一般都需要通过流来获取，我们可以使用request.getInputStream()，原生态的文件上传流被获取，十分麻烦
+        //但是我们都建议使用Apache的文件上传组件来实现，common-fileupload，它需要依赖common-io组件
+
+        //1.创建
     }
 
     @Override
